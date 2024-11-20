1e2a47d38e74190208284c2b80a3a02442bcc346
RazniSliit <it24103841@my.sliit.lk>
2024-11-20 22:43:55 +0530

    Add files via upload

---
+++ Q1B/IT24103841Lab3Q1B.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class IT24103841Lab3Q1B 
+{
+public static void main(String[] args) 
+{
+Scanner scanner=new Scanner(System.in);
+System.out.print("Enter the price of 1kg of rice:");
+double pricePerKg=scanner.nextDouble();
+System.out.print("Enter the number of kilograms you want to buy:");
+double kilograms=scanner.nextDouble();
+double totalAmount=pricePerKg*kilograms;
+double discount=totalAmount*0.10;
+double finalAmount=totalAmount-discount;
+System.out.println("The total amount with 10% discount is:"+finalAmount);
+scanner.close();
+
+              }
+  
+}
+              
+              
